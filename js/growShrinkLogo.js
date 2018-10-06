67727c225cf0de55cc0d456b72957cd80b4eb801
rajitha302 <you@example.com>
2018-10-06 12:38:21 +0530

    navbar logo height fixed auto resizing disabled

--- js/growShrinkLogo.js
+++ js/growShrinkLogo.js
@@ -2,17 +2,7 @@ window.onscroll = function () {
     growShrinkLogo();
 };
 function growShrinkLogo() {
-    var Logo = document.getElementById("Logo");
-    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
-        Logo.style.height = '25px';
-        Logo.style.width = '90%';
-        Logo.style.transitionDuration = '0.6s';
-    }
-    else {
-        Logo.style.height = '30px';
-        Logo.style.width = '100%';
-    }
-
+    
    var nav = document.getElementById("nav");
    if (document.body.scrollTop > 5 || document.documentElement.scrollTop > 5) {
       nav.style.boxShadow = '0 8px 6px -6px #999';
